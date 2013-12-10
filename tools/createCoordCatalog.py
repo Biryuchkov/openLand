2cf96558bace8733a653d5725028ef2b5c4ddcba
filippov70 <filippov70@gmail.com>
2013-12-10 18:36:53 +0700

    Теперь можно настраивать размер шрифта (с самим шрифтом ещё не определился) от xx-small до large. По дефолту используется small (значение 2). Добавлен диалог сохранения файла. Запоминается последний путь.

--- tools/createCoordCatalog.py
+++ tools/createCoordCatalog.py
@@ -6,8 +6,8 @@ __author__ = 'Filippov Vladislav'
 
 from PyQt4 import QtCore, QtGui
 from coordcatalog import CatalogData
-import os
-from PyQt4.QtGui import QDialog, QMessageBox
+import os.path
+from PyQt4.QtGui import QDialog, QMessageBox, QFileDialog
 from createCoordCatalog_ui import Ui_CoordCatalog
 # открывать html-файлы с помощью браузера. корректно показывает Firefox
 
@@ -20,7 +20,7 @@ class CreateCoordCatalog(QDialog, Ui_CoordCatalog):
 		QDialog.__init__(self, iface.mainWindow())
 		self.iface = iface
 		self.setupUi(self)
-
+		self.curr_path = u''
 		self.connect(self.btnCreateCoord, QtCore.SIGNAL("clicked()"), self.calculate)
 		self.connect(self.btnSave, QtCore.SIGNAL("clicked()"), self.save_catalog)
 
@@ -39,8 +39,13 @@ class CreateCoordCatalog(QDialog, Ui_CoordCatalog):
 			#                    QtGui.QMessageBox.Ok)
 
 	def save_catalog(self):
-		current_path = os.path.dirname(__file__)
-		filepath = os.path.join(current_path, 'coord_catalog.html')
-		ccf = open(filepath, 'w')
-		ccf.write(self.html_cataloga_data.encode('utf8'))
-		ccf.close()
+		if self.curr_path == u'':
+			self.curr_path = os.getcwd()
+		file_name = QFileDialog.getSaveFileName(self, u'Сохраните данные', self.curr_path, u'HTML файлы(*.html *.HTML)')
+		if not file_name is None or not file_name == u'':
+			current_path = os.path.dirname(unicode(file_name))
+			self.curr_path = current_path
+			filepath = file_name  #os.path.join(current_path, '.html')
+			ccf = open(filepath, 'w')
+			ccf.write(self.html_cataloga_data.encode('utf8'))
+			ccf.close()
