69afe46f489f0ff19522d4fa1946986c6cc806a9
filippov <filippov70@gmail.com>
2014-02-06 00:02:48 +0700

    Инструмент создания полигонов по точкам умеет создавать одноконтурные полигоны

--- tools/createpolygonfrompoint.py
+++ tools/createpolygonfrompoint.py
@@ -8,27 +8,32 @@ from common import *
 
 
 class CreatePolygonFromPoints():
-	def __init__(self, iface):
-		self.iface = iface
+    def __init__(self, iface):
+        self.iface = iface
 
-	def createpolygon(self):
-		parcellayer = get_vector_layer_by_name(u'Участок')
-		idMsk = idCurrentMSK()
-		if (self.iface.mapCanvas().currentLayer() is not None) \
-			and (self.iface.mapCanvas().currentLayer().selectedFeatures() is not None):
-			points = self.iface.mapCanvas().currentLayer().selectedFeatures()
-			pgeom = []
-			for point in points:
-				pgeom.append(point.geometry().asPoint())
+    def createpolygon(self):
+        parcellayer = get_vector_layer_by_name(u'Участок')
+        idMsk = idCurrentMSK()
+        if (self.iface.mapCanvas().currentLayer() is not None) \
+            and (self.iface.mapCanvas().currentLayer().selectedFeatures() is not None):
+            points = self.iface.mapCanvas().currentLayer().selectedFeatures()
+            pgeom = []
+            for point in points:
+                geom = point.geometry().asPoint()
+                pgeom.append(QgsPoint(geom.x(), geom.y()))
 
-			if not parcellayer is None:
-				if insertFeatures('ln_uchastok', ['id_msk', 'geom'], [idMsk, QgsGeometry.fromPolygon([pgeom])]):
-					pass
-				else:
-					QMessageBox.warning(self.iface.mainWindow(), u'Внимание!',
-					u'Не удалось добавить участок')
-		else:
-			pass
+            if not parcellayer is None:
+                pg = QgsGeometry.fromPolygon([pgeom])
+                feat = QgsFeature()
+                feat.initAttributes(len(parcellayer.dataProvider().attributeIndexes()))
+                feat.setAttribute(parcellayer.fieldNameIndex('id_msk'), idMsk)
+                feat.setGeometry(pg)
+                if parcellayer.dataProvider().addFeatures([feat]):
+                    pass
+                else:
+                    QMessageBox.warning(self.iface.mainWindow(), u'Внимание!', u'Не удалось добавить участок')
+        else:
+            QMessageBox.warning(self.iface.mainWindow(), u'Внимание!', u'Нет слоя \'Участок\'. Негде создавать участок')
 
-	def createmultipolygon(self):
-		pass
+    def createmultipolygon(self):
+        pass
